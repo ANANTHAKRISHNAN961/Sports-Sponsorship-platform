648e8b6af63f734cef8a569accc8a44eda352ab3
ANANTHAKRISHNAN961 <ananthakrishnans961@gmail.com>
2024-10-07 22:39:39 +0530

    removed the older code

--- models/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = new mongoose.Schema({
-  athlete: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Post', PostSchema);
